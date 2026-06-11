127ddf22082dc4a8bfe3d9ad83a756e464eced1e
Huskar-Silencer <jammersolar27@gmail.com>
2026-06-11 15:29:01 +0800

    feat: delete files

--- utils/constants.py
+++
@@ -1,9 +0,0 @@
-"""Application constants"""
-
-APP_TITLE = "SimImageView"
-APP_VERSION = "1.0.0"
-
-SUPPORTED_FORMATS = {
-    ".jpg", ".jpeg", ".png", ".bmp",
-    ".gif", ".webp", ".tiff", ".ico"
-}
